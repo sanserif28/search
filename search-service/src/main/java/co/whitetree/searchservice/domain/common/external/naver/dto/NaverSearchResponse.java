72684649717b8a776bad29fffc0d083f132f9de0
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2022-01-09 03:48:20 +0900

    fix: restTemplate 예외 던지는 대신 null 반환

--- search-service/src/main/java/co/whitetree/searchservice/domain/common/external/naver/dto/NaverSearchResponse.java
+++ search-service/src/main/java/co/whitetree/searchservice/domain/common/external/naver/dto/NaverSearchResponse.java
@@ -4,13 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 public class NaverSearchResponse {
     @JsonProperty("items")
-    private List<Item> items;
+    private List<Item> items = new ArrayList<>();
 
     @Getter
     @Setter
